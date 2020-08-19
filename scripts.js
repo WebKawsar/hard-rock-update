e65d0b26cf02f29318fd64f08a4147c98624d9f7
WebKawsar <web.kawsarahmed@gmail.com>
2020-08-19 09:56:19 +0600

    initial setup

---
+++ scripts.js
@@ -0,0 +1,77 @@
+const numbers = [10, 9, 25, 64, 49];
+
+
+
+// const results = numbers.map(function(element, index, array){
+//     console.log(element * element, index, array); 
+// });
+
+// console.log(results);
+
+
+
+// const results2 = numbers.map((element, index, array) =>{
+//     console.log(element * element, index, array);
+// } );
+
+// console.log(results2);
+
+
+// const results3 = numbers.forEach(function(element, index, array){
+//     console.log(element * element);
+//     console.log(index, array);
+// });
+
+// const results4 = numbers.reduce(function(total=50, elemnt, index, array){
+   
+//     return(total + elemnt);
+  
+
+// });
+
+// console.log(results4);
+
+
+
+
+const students = [
+    {id:101, name:"Kawsar Ahmed"},
+    {id:102, name:"Joly Begum"},
+    {id:103, name:"Mily Begum"},
+    {id:104, name:"Beauty Begum"},
+    {id:105, name:"Sewly Begum"}
+]
+
+// for (let i = 0; i < students.length; i++) {
+//     const element = students[i];
+//     console.log(element.name, element.id);
+    
+// }
+
+// students.map(function(element){
+//     console.log(element.name, element.id);
+// });
+
+// const results5 = students.map(element => element.name + element.id);
+
+// console.log(results5);
+
+
+
+numbers.map(function(element, index, array){
+
+    console.log(element, index, array);
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
